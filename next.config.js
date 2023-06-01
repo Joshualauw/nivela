26207046bdb99707fa6d7101b253b4be33b93c52
Joshualauw <joshualauw1@gmail.com>
2023-06-01 13:08:53 +0700

    (fix): attempt #3 to fix file upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["nivela.vercel.app", "localhost"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
